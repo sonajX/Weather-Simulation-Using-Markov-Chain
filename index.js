d6304ed2143873054501cd6f4ea6e3fc88f22aac
sonajX <jwalmendra@gmail.com>
2025-01-30 21:46:12 +0800

    Added sample data to work on and fixed id labels.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const sampleData = [
   { firstCol: "Row 4", pR: "10%", pS: "40%", pC: "50%" }
 ];
 
+const sunnyToCloudy = document.getElementById
+
 function populateTable(data) {
   const tableBody = document.querySelector("#results-table tbody") || document.createElement("tbody");
 
@@ -22,4 +24,37 @@ function populateTable(data) {
   document.getElementById("results-table").appendChild(tableBody);
 }
 
-populateTable(sampleData);
+function simulate(){
+  populateTable(sampleData);
+}
+
+
+
+const matrix3by3 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+const matrix3by1 = [
+  [1],
+  [2],
+  [3]
+];
+
+function multiplyMatrix(probabilityMatrix, initialState) {
+  const result = [];
+  
+  for (let i = 0; i < 3; i++) {
+    let sum = 0;
+    for (let j = 0; j < 3; j++) {
+      sum += probabilityMatrix[i][j] * initialState[j][0];
+    }
+    result.push([sum]);
+  }
+  
+  return result;
+}
+
+const resultMatrix = multiplyMatrix(matrix3by3, matrix3by1);
+console.log("Result of 3x3 * 3x1 matrix multiplication:", resultMatrix);
